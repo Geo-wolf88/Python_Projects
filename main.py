522e3d86ccb602d5db90d5082d1e651f20ecdc0f
Geo-wolf88 <82673499+Geo-wolf88@users.noreply.github.com>
2022-05-26 10:25:58 +0300

    InternetTest

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import qrcode
+import speedtest
 
 
-img = qrcode.make ('https://www.youtube.com/watch?v=04854XqcfCY')
-img.save('myQRcode.png')
-img.show()
+def test():
+    s = speedtest.Speedtest()
+    s.get_server()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    res = s.result.dict()
+    return res["download"], res["upload"], res["ping"]
 
 
+def  main():
+
+   for i in range(3):
+       d,u,p=test()
+
+   print("Viteza de descarcare wifi".format(d/1024))
+   print("Viteza de incarcare wifi".format(u/1024))
+   print("Ping".format(p))
+
+if __name__ == "__main__":
+    main()
